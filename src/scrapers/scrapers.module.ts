e677f37ed5d816e08c14a5d42fb1c8690894b11f
kilhyeonjun <kboxstar@gmail.com>
2024-08-09 08:39:59 +0900

    chore: ScrapersController 등록

--- src/scrapers/scrapers.module.ts
+++ src/scrapers/scrapers.module.ts
@@ -4,9 +4,11 @@ import { JobPostsModule } from '../job-posts/job-posts.module';
 import { scrapers } from './strategies';
 import { SCRAPING_STRATEGIES } from './interfaces/scraping-strategy.interface';
 import { ScrapersScheduler } from './scrapers.scheduler';
+import { ScrapersController } from './scrapers.controller';
 
 @Module({
   imports: [JobPostsModule],
+  controllers: [ScrapersController],
   providers: [
     ...scrapers,
     {
