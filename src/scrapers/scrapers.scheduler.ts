3e7d460da146ea93635ae48d0ee67a339ba4e4a9
kilhyeonjun <kboxstar@gmail.com>
2024-08-13 18:09:03 +0900

    chore: 공고 scrape 전체로 변경

--- src/scrapers/scrapers.scheduler.ts
+++ src/scrapers/scrapers.scheduler.ts
@@ -1,8 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { ScrapersService } from './scrapers.service';
-import {} from './interfaces/scraping-strategy.interface';
-import { HospitalName } from '../common/enums/hospital-name.enum';
 
 @Injectable()
 export class ScrapersScheduler {
@@ -11,9 +9,9 @@ export class ScrapersScheduler {
   constructor(private readonly scrapersService: ScrapersService) {}
 
   @Cron(CronExpression.EVERY_HOUR)
-  async scrapeSeverance() {
+  async scrapeAll() {
     this.logger.debug('Processing scrapers');
 
-    await this.scrapersService.scrapeOne(HospitalName.Severance);
+    await this.scrapersService.scrapeAll();
   }
 }
