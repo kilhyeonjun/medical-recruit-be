02dfdfb73f4fe312f4c8d2a264e8f803d7ac3f51
kilhyeonjun <kboxstar@gmail.com>
2024-08-14 09:05:37 +0900

    wip

--- src/scrapers/strategies/index.ts
+++ src/scrapers/strategies/index.ts
@@ -1,7 +1,7 @@
 import { CaumcScrapingStrategy } from './caumc-scraping.strategy';
-import { ChCauhsScrapingStrategy } from './chCauhs-scraping.strategy';
 import { EumcScrapingStrategy } from './eumc-scraping.strategy';
 import { SeveranceScrapingStrategy } from './severance-scraping.strategy';
+import { ChCauhsScrapingStrategy } from './temp-chCauhs-scraping.strategy';
 
 export const scrapers = [
   SeveranceScrapingStrategy,
